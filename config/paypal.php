9eba5e94f916c82d02b3ecf84f3b147f731d3f70
Rama <kazureisuke@gmail.com>
2018-09-28 10:19:52 +0700

    u

---
+++ config/paypal.php
@@ -0,0 +1,12 @@
+<?php 
+return [ 
+    'client_id' => env('PAYPAL_CLIENT_ID','AdvLnbeWxPgPcMaYxcdPKvE93QvuDnRDBpzQkZim-aIJx0v0H6UVvE-yJCYT-MG7onNteqQ-wlo67RRX'),
+    'secret' => env('PAYPAL_SECRET','EDVwpDG3PFUiAjdMHoNuCAwimUjIv6IhftaPPWHm-G8-XESbqjS9vcMoWnEUjiBtIr4O0FtAg0i8cFzf'),
+    'settings' => array(
+        'mode' => env('PAYPAL_MODE','sandbox'),
+        'http.ConnectionTimeOut' => 30,
+        'log.LogEnabled' => true,
+        'log.FileName' => storage_path() . '/logs/paypal.log',
+        'log.LogLevel' => 'ERROR'
+    ),
+];
