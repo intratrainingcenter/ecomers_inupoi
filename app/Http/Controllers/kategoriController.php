08b7465d604ecd46327ef019813f019a17fd9fcb
bagas <bagassigit24@gmail.com>
2018-10-04 02:24:13 +0700

    jhj

--- app/Http/Controllers/kategoriController.php
+++ app/Http/Controllers/kategoriController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class kategoriController extends Controller
+class KategoriController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class kategoriController extends Controller
      */
     public function index()
     {
-        return view('content.kategori');
+        //
     }
 
     /**
