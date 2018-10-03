d8bb9ee9deae7cc23f30d596694e7fec2bc75f3f
obed ridho <deboabilla@gmail.com>
2018-10-03 16:26:51 +0700

    perbaikan notifikasi
    -

--- resources/views/content/kategori/kategori.blade.php
+++ resources/views/content/kategori/kategori.blade.php
@@ -28,25 +28,25 @@ $(function() {
   <div class="x_panel">
     <div class="x_content">
 @if ($message = Session::get('success'))
-    <div class="alert alert-success alert alert-success alert-dismissible fade in" role="alert" >
+    <div class="alert alert-success alert alert-success alert-dismissible fade in notif" role="alert" >
       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
       </button>
         <p>{{ $message }}</p>
     </div>
     @elseif ($message = Session::get('edit'))
-    <div class="alert alert-warning alert alert-warning alert-dismissible fade in" role="alert" >
+    <div class="alert alert-warning alert alert-warning alert-dismissible fade in notif" role="alert" >
       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
       </button>
         <p>{{ $message }}</p>
     </div>
     @elseif ($message = Session::get('delete'))
-    <div class="alert alert-danger alert alert-danger alert-dismissible fade in" role="alert" >
+    <div class="alert alert-danger alert alert-danger alert-dismissible fade in notif" role="alert" >
       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
       </button>
         <p>{{ $message }}</p>
     </div>
     @elseif ($message = Session::get('not_success'))
-    <div class="alert alert-danger alert alert-danger alert-dismissible fade in" role="alert" >
+    <div class="alert alert-danger alert alert-danger alert-dismissible fade in notif" role="alert" >
       <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span>
       </button>
         <p>{{ $message }}</p>
@@ -125,6 +125,4 @@ $(function() {
   </div>
   <!-- /.modal-dialog -->
 </div>
-
 @endsection
-
