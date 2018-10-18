cd42443edea3d4be944809921b9570828b6d18b1
obed ridho <deboabilla@gmail.com>
2018-10-18 14:14:24 +0700

    perbaikan cart frontend
    -

--- resources/views/frondend/header/header.blade.php
+++ resources/views/frondend/header/header.blade.php
@@ -29,8 +29,7 @@
 							</li>
 
 							<li>
-								<a href="{{Route('Inupoi.Transaksi')}}">
-transaction</a>
+								<a href="{{Route('Inupoi.Transaksi')}}">transaction</a>
 							</li>
 
 							<li>
@@ -49,13 +48,13 @@ transaction</a>
 							<i class="zmdi zmdi-search"></i>
 						</div>
 
-						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
+						<a href="{{Route('Inupoi.Transaksi')}}" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="2">
 							<i class="zmdi zmdi-shopping-cart"></i>
-						</div>
-
-						<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
-							<i class="zmdi zmdi-favorite-outline"></i>
 						</a>
+
+						<div class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="0">
+							<i class="zmdi zmdi-favorite-outline"></i>
+						</div>
 					</div>
 				</nav>
 			</div>	
