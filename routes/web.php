9312d8665fdd11741f58bd91342e5ecc1ab45b14
Rama <kazureisuke@gmail.com>
2018-10-18 15:53:53 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -22,46 +22,46 @@ Route::prefix('Inupoi')->group(function(){
 	Route::get('/Produk', 'frondend\FrondendController@produk')->name('Inupoi.Produk');
 	Route::get('/Transaksi', 'frondend\FrondendController@transaksi')->name('Inupoi.Transaksi');
 	Route::get('/Detail', 'frondend\FrondendController@detail')->name('Inupoi.Detail');
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
-Route::get('Inupoi/{provider}', 'Auth\LoginController@redirectToProvider');
-Route::get('Inupoi/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
-Route::resource('Inupoi', 'FrondendController');
-Route::resource('dashboard','dashboardController')->middleware('auth');
-Route::resource('kategori','KategoriController')->middleware('auth');
-Route::resource('barang','ProdukController')->middleware('auth');
-Route::resource('keranjang','KeranjangController')->middleware('auth');
-Route::resource('diskon','DiskonController')->middleware('auth');
-Route::resource('retur','ReturController')->middleware('auth');
-Route::resource('komentar','KomentarController')->middleware('auth');
-Route::resource('laporanbarang','LabBarangController')->middleware('auth');
-Route::resource('laporankeuangan','LapKeuanganController')->middleware('auth');
-Route::resource('laporantransaksi','LapTransaksiController')->middleware('auth');
-Route::resource('user','UserController')->middleware('auth');
-Route::resource('setting','SettingController')->middleware('auth');
-Route::resource('user','UserController')->middleware('auth');
-Route::resource('userprofile','UserprofileController')->middleware('auth');
-Route::resource('kategori','kategoriController')->middleware('auth');
-
 });
 
 Route::prefix('laporankeuangan')->group(function(){
 	Route::get('/Filter', 'LapKeuanganController@filter')->name('Filter.laporankeuangan');
 });
 
-Route::resource('Inupoi', 'frondend\FrondendController');
-Route::resource('dashboard','backend\dashboardController');
-Route::resource('barang','backend\ProdukController');
-Route::resource('keranjang','backend\KeranjangController');
-Route::resource('diskon','backend\DiskonController');
-Route::resource('retur','backend\ReturController');
-Route::resource('komentar','backend\KomentarController');
-Route::resource('laporanbarang','backend\LabBarangController');
-Route::resource('laporankeuangan','backend\LapKeuanganController');
-Route::resource('laporantransaksi','backend\LapTransaksiController');
-Route::resource('user','backend\UserController');
-Route::resource('setting','backend\SettingController');
-Route::resource('user','backend\UserController');
-Route::resource('userprofile','backend\UserprofileController');
-Route::resource('kategori','backend\kategoriController');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+Route::get('Inupoi/{provider}', 'Auth\LoginController@redirectToProvider');
+Route::get('Inupoi/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+Route::resource('Inupoi', 'FrondendController');
+Route::resource('dashboard','backend\dashboardController')->middleware('auth');
+Route::resource('kategori','backend\KategoriController')->middleware('auth');
+Route::resource('barang','backend\ProdukController')->middleware('auth');
+Route::resource('keranjang','backend\KeranjangController')->middleware('auth');
+Route::resource('diskon','backend\DiskonController')->middleware('auth');
+Route::resource('retur','backend\ReturController')->middleware('auth');
+Route::resource('komentar','backend\KomentarController')->middleware('auth');
+Route::resource('laporanbarang','backend\LabBarangController')->middleware('auth');
+Route::resource('laporankeuangan','backend\LapKeuanganController')->middleware('auth');
+Route::resource('laporantransaksi','backend\LapTransaksiController')->middleware('auth');
+Route::resource('setting','backend\SettingController')->middleware('auth');
+Route::resource('user','backend\UserController')->middleware('auth');
+Route::resource('userprofile','backend\UserprofileController')->middleware('auth');
 Route::resource('fpro', 'frondend\FrontProductController');
+
+
+
+Route::resource('Inupoi', 'frondend\FrondendController');
+// Route::resource('dashboard','backend\dashboardController');
+// Route::resource('barang','backend\ProdukController');
+// Route::resource('keranjang','backend\KeranjangController');
+// Route::resource('diskon','backend\DiskonController');
+// Route::resource('retur','backend\ReturController');
+// Route::resource('komentar','backend\KomentarController');
+// Route::resource('laporanbarang','backend\LabBarangController');
+// Route::resource('laporankeuangan','backend\LapKeuanganController');
+// Route::resource('laporantransaksi','backend\LapTransaksiController');
+// Route::resource('user','backend\UserController');
+// Route::resource('setting','backend\SettingController');
+// Route::resource('user','backend\UserController');
+// Route::resource('userprofile','backend\UserprofileController');
+// Route::resource('kategori','backend\kategoriController');
 
