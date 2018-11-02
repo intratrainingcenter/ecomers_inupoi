197ca8ad8b6daf752a64f282dfc11371b32198da
obed ridho <deboabilla@gmail.com>
2018-11-02 13:21:46 +0700

    tes
    -

--- resources/views/frondend/header/header.blade.php
+++ resources/views/frondend/header/header.blade.php
@@ -64,9 +64,13 @@
 							<i class="zmdi zmdi-search"></i>
 						</div>
 
-						<a href="{{Route('Inupoi.Transaksi')}}" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="2">
+						{{-- <a href="{{Route('Inupoi.Transaksi')}}" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="2">
 							<i class="zmdi zmdi-shopping-cart"></i>
-						</a>
+						</a> --}}
+
+						<div class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
+							<i class="zmdi zmdi-shopping-cart"></i>
+						</div>
 
 						<div class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="0">
 							<i class="zmdi zmdi-favorite-outline"></i>
