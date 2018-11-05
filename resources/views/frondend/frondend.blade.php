fcdf76a661960f779f59fe7ab8d22cc9671f7823
bagas <bagassigit24@gmail.com>
2018-11-05 14:36:38 +0700

    -

---
+++ resources/views/frondend/frondend.blade.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="{{asset('cozastore/images/icons/favicon.png')}}"/>
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{asset('cozastore/images/icons/log.png')}}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/bootstrap/css/bootstrap.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/fonts/iconic/css/material-design-iconic-font.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/fonts/linearicons-v1.0.0/icon-font.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/animate/animate.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/css-hamburgers/hamburgers.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/animsition/css/animsition.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/select2/select2.min.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/daterangepicker/daterangepicker.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/slick/slick.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/MagnificPopup/magnific-popup.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/vendor/perfect-scrollbar/perfect-scrollbar.css')}}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/css/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('cozastore/css/main.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/imgproduk.css')}}">
+<!--===============================================================================================-->
+</head>
+<body class="animsition">
+
+	<!-- Header -->
+	<header>
+		@include('frondend.header.header')
+	</header>
+
+	<!-- Cart -->
+	@include('frondend.keranjang.cart')	
+
+	<!-- Slider -->
+	@include('frondend.slider.slider')
+
+
+	<!-- Banner -->
+	<div class="sec-banner bg0 p-t-80 p-b-50">
+		@include('frondend.banner.banner')
+	</div>
+
+
+	<!-- Product -->
+	<section class="bg0 p-t-23 p-b-140">
+		<div class="container">
+			<div class="p-b-10">
+				<h3 class="ltext-103 cl5">
+					Product Overview
+				</h3>
+			</div>
+
+			<div class="flex-w flex-sb-m p-b-52">
+				<div class="flex-w flex-l-m filter-tope-group m-tb-10">
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
+						All Products
+					</button>
+
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".women">
+						Women
+					</button>
+
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".men">
+						Men
+					</button>
+
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".bag">
+						Bag
+					</button>
+
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".shoes">
+						Shoes
+					</button>
+
+					<button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".watches">
+						Watches
+					</button>
+				</div>
+			</div>
+
+			<div class="row isotope-grid">
+				@include('frondend.produk.produk')
+			</div>
+
+			<!-- Load more -->
+			<div class="flex-c-m flex-w w-full p-t-45">
+				<a href="#" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
+					Load More
+				</a>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Footer -->
+	<footer class="bg3 p-t-75 p-b-32">
+		@include('frondend.footer.footer')
+	</footer>
+
+
+	<!-- Back to top -->
+	<div class="btn-back-to-top" id="myBtn">
+		<span class="symbol-btn-back-to-top">
+			<i class="zmdi zmdi-chevron-up"></i>
+		</span>
+	</div>
+
+	<!-- Modal1 -->
+	<div class="wrap-modal1 js-modal1 p-t-60 p-b-20">
+		@include('frondend.modal.modal')
+	</div>
+
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/animsition/js/animsition.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/bootstrap/js/popper.js')}}"></script>
+	<script src="{{asset('cozastore/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/select2/select2.min.js')}}"></script>
+	<script>
+		$(".js-select2").each(function(){
+			$(this).select2({
+				minimumResultsForSearch: 20,
+				dropdownParent: $(this).next('.dropDownSelect2')
+			});
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/daterangepicker/moment.min.js')}}"></script>
+	<script src="{{asset('cozastore/vendor/daterangepicker/daterangepicker.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/slick/slick.min.js')}}"></script>
+	<script src="{{asset('cozastore/js/slick-custom.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/parallax100/parallax100.js')}}"></script>
+	<script>
+        $('.parallax100').parallax100();
+	</script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/MagnificPopup/jquery.magnific-popup.min.js')}}"></script>
+	<script>
+		$('.gallery-lb').each(function() { // the containers for all your galleries
+			$(this).magnificPopup({
+		        delegate: 'a', // the selector for gallery item
+		        type: 'image',
+		        gallery: {
+		        	enabled:true
+		        },
+		        mainClass: 'mfp-fade'
+		    });
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/isotope/isotope.pkgd.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/sweetalert/sweetalert.min.js')}}"></script>
+	<script>
+		function countwishlist(){
+			$.ajax({
+				headers:{
+					'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content'),
+				},
+				method:'get',
+				url:location.origin+'/Inupoi/CountFavorite/',
+				success:function(data){
+					$('.js-show-cart').attr('data-notify',data)
+				}
+			});
+		}
+
+		$(document).ready(function() {
+			countwishlist();
+		});
+
+		$('.js-addwish-b2').on('click', function(e){
+			e.preventDefault();
+			let nameProduct = $(this).parent().parent().find('.js-name-b2').html();
+			let kodeproduk = $(this).attr('kdProduk');
+			if($(this).parent().find('.js-addedwish-b2').length == 0){
+				$.ajax({
+					headers:{
+						'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content'),
+					},
+					method:'get',
+					url:location.origin+'/Inupoi/Favorite/'+kodeproduk,
+					success:function(data){
+						swal(nameProduct, "is added to wishlist !", "success");
+					}
+				});
+
+				countwishlist();
+				$(this).addClass('js-addedwish-b2');
+			}else{
+				$.ajax({
+					headers:{
+						'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content'),
+					},
+					method:'get',
+					url:location.origin+'/Inupoi/RemoveFavorite/'+kodeproduk,
+					success:function(data){
+						swal(nameProduct, "is remove to wishlist !", "success");
+					}
+				});
+
+				countwishlist();
+				$(this).removeClass('js-addedwish-b2');
+			}
+		});
+
+		$('.js-addwish-detail').each(function(){
+			var nameProduct = $(this).parent().parent().parent().find('.js-name-detail').html();
+
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to wishlist !", "success");
+
+				$(this).addClass('js-addedwish-detail');
+				$(this).off('click');
+			});
+		});
+
+		/*---------------------------------------------*/
+
+		$('.js-addcart-detail').each(function(){
+			var nameProduct = $(this).parent().parent().parent().parent().find('.js-name-detail').html();
+			$(this).on('click', function(){
+				swal(nameProduct, "is added to cart !", "success");
+			});
+		});
+
+	</script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/vendor/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+	<script>
+		$('.js-pscroll').each(function(){
+			$(this).css('position','relative');
+			$(this).css('overflow','hidden');
+			var ps = new PerfectScrollbar(this, {
+				wheelSpeed: 1,
+				scrollingThreshold: 1000,
+				wheelPropagation: false,
+			});
+
+			$(window).on('resize', function(){
+				ps.update();
+			})
+		});
+
+		
+	</script>
+<!--===============================================================================================-->
+	<script src="{{asset('cozastore/js/main.js')}}"></script>
+
+
+	 {{-- onesignal --}}
+	 <link rel="manifest" href="/manifest.json" />
+	 <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+	 <script>
+	   var OneSignal = window.OneSignal || [];
+	   OneSignal.push(function() {
+		 OneSignal.init({
+		   appId: "48fefe3b-d8be-42be-b43a-2ca3832e0f43",
+		   autoRegister: false,
+		   notifyButton: {
+			 enable: true,
+		   },
+		   allowLocalhostAsSecureOrigin: true,
+		 });
+	   });
+	 </script>
+	 {{-- end onesignal --}}
+
+</body>
+</html>
